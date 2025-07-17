67eed3e16c19ceac9c1dfe64f0b3c2c7110f7c4e
Bijan "Biji" Camp <8800015+bijancamp@users.noreply.github.com>
2025-07-17 18:08:15 +0000

    Finish Section 3

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,16 +1,53 @@
+import { useState } from 'react';
+
+import CoreConcept from './components/CoreConcept';
+import Header from './components/Header/Header';
+import TabButton from './components/TabButton';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
+
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map(concept => (
+              <CoreConcept key={concept.title} {...concept} />
+            ))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            {Object.keys(EXAMPLES).map(topic => (
+              <TabButton
+                key={topic}
+                onSelect={() => handleSelect(topic)}
+                isSelected={selectedTopic === topic}
+              >
+                {EXAMPLES[topic].title}
+              </TabButton>
+            ))}
+          </menu>
+          {!selectedTopic && <p>Please select a topic.</p>}
+          {selectedTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
+        </section>
       </main>
     </div>
   );
