6e69ddf80c671d55e44dba59878c74162e6ccdf4
Bijan "Biji" Camp <8800015+bijancamp@users.noreply.github.com>
2025-07-18 04:15:15 +0000

    Finish React Essentials site (Section 4)

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,55 +1,16 @@
-import { useState } from 'react';
-
-import CoreConcept from './components/CoreConcept';
+import CoreConcepts from './components/CoreConcepts';
+import Examples from './components/Examples';
 import Header from './components/Header/Header';
-import TabButton from './components/TabButton';
-import { CORE_CONCEPTS, EXAMPLES } from './data';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
-
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map(concept => (
-              <CoreConcept key={concept.title} {...concept} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            {Object.keys(EXAMPLES).map(topic => (
-              <TabButton
-                key={topic}
-                onSelect={() => handleSelect(topic)}
-                isSelected={selectedTopic === topic}
-              >
-                {EXAMPLES[topic].title}
-              </TabButton>
-            ))}
-          </menu>
-          {!selectedTopic && <p>Please select a topic.</p>}
-          {selectedTopic && (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
